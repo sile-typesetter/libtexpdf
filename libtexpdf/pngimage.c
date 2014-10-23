8f7de6f5a0e8a1aea87b9680cf0309ac67651112
Simon Cozens <simon@simon-cozens.org>
2014-10-23 14:34:43 +0900

    Build cleanly on libpng1.2

--- libtexpdf/pngimage.c
+++ libtexpdf/pngimage.c
@@ -20,8 +20,6 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
 
-#include "libtexpdf.h"
-
 /*
  * PNG SUPPORT
  *
@@ -62,6 +60,8 @@
 #endif
 
 #include <png.h>
+#include "libtexpdf.h"
+
 #include "pngimage.h"
 
 #include "pdfximage.h"
