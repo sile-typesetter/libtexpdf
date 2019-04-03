02d17f294a8e0c005efe99b37d64ba95f8656315
Rui Xia <ruixia@microsoft.com>
2019-04-03 00:00:29 -0700

    Add CMake building script and config.h CMake template.

---
+++ config.h.cmake
@@ -0,0 +1,56 @@
+/* Define to 1 if you have the `getenv' function. */
+#cmakedefine HAVE_GETENV @HAVE_GETENV@
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H @HAVE_INTTYPES_H@
+
+/* Define if you have libpng and its headers. */
+#cmakedefine HAVE_LIBPNG @HAVE_LIBPNG@
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H @HAVE_MEMORY_H@
+
+/* Define to 1 if you have the `mkstemp' function. */
+#cmakedefine HAVE_MKSTEMP @HAVE_MKSTEMP@
+
+/* Define to 1 if you have the <stdbool.h> header file. */
+#cmakedefine HAVE_STDBOOL_H @HAVE_STDBOOL_H@
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H @HAVE_STDINT_H@
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H @HAVE_STDLIB_H@
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H @HAVE_STRING_H@
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H @HAVE_SYS_STAT_H@
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H @HAVE_SYS_TYPES_H@
+
+/* Define to 1 if you have the <sys/wait.h> header file. */
+#cmakedefine HAVE_SYS_WAIT_H @HAVE_SYS_WAIT_H@
+
+/* Define if <time.h> has timezone as an external variable. */
+#cmakedefine HAVE_TIMEZONE @HAVE_TIMEZONE@
+
+/* Define if struct tm has tm_gmtoff as a member. */
+#cmakedefine HAVE_TM_GMTOFF @HAVE_TM_GMTOFF@
+
+/* Define if you have zlib and its headers. */
+#cmakedefine HAVE_ZLIB @HAVE_ZLIB@
+
+/* Define if your zlib has the compress2 function. */
+#cmakedefine HAVE_ZLIB_COMPRESS2 @HAVE_ZLIB_COMPRESS2@
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME @PACKAGE_NAME@
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION @PACKAGE_VERSION@
+
+/* The size of `long', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
