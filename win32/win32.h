93998b479bba21ed68305096eda8cd73bcf59408
Francesco Abbate <francesco.bbt@gmail.com>
2023-01-06 11:23:03 +0100

    Fix build on Windows

--- win32/win32.h
+++ win32/win32.h
@@ -1,7 +1,10 @@
 #pragma once
-#ifdef WIN32
 #define localtime_r(timep, result) localtime_s(result, timep)
 #define gmtime_r(timep, result) gmtime_s(result, timep)
 #define strncasecmp _strnicmp
+#if defined(_MSC_VER)
+#define off64_t int64_t
+#define ftello _ftelli64
+#define fseeko _fseeki64
 #define stat _stat
 #endif
